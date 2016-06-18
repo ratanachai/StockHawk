fb1a34b72e90ad397561a9c97276c21a517edfc4
Ratanachai S <ratanachai@gmail.com>
2016-06-19 07:33:12 +1200

    Add clicking on header to launch MyStocksActivity

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/StocksWidgetProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StocksWidgetProvider.java
@@ -1,5 +1,6 @@
 package com.sam_chordas.android.stockhawk.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
@@ -9,6 +10,7 @@ import android.widget.RemoteViews;
 
 import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.service.StockTaskService;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
 
 /**
  * Created by keng on 30/05/16.
@@ -19,7 +21,12 @@ public class StocksWidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int wid : appWidgetIds) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_stocks);
-            // Set up the collection
+            // Click on header to launch main activity
+            Intent intent = new Intent(context, MyStocksActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget_header, pendingIntent);
+
+            // Set up the collection data (ListView)
             views.setRemoteAdapter(R.id.widget_listview, new Intent(context, StocksWidgetService.class));
             views.setEmptyView(R.id.widget_listview, R.id.widget_empty);
             appWidgetManager.updateAppWidget(wid, views);
