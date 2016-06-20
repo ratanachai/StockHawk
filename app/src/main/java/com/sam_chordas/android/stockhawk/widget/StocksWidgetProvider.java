48d6665bb7ac3135c6d0d3b63a24ddf1f84f56df
Ratanachai S <ratanachai@gmail.com>
2016-06-21 11:07:39 +1200

    Show/Hide a column of data according to Widget Width

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/StocksWidgetProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StocksWidgetProvider.java
@@ -6,6 +6,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
 import com.sam_chordas.android.stockhawk.R;
@@ -19,18 +20,33 @@ public class StocksWidgetProvider extends AppWidgetProvider {
     private static final String LOG_TAG = StocksWidgetProvider.class.getSimpleName();
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int wid : appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_stocks);
+
+            // Widget ID needed to determine Widget width in getViewAt
+            Intent fillData = new Intent(context, StocksWidgetService.class);
+            fillData.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+
+            // Set up the RemoteView with collection data (ListView)
+            views.setRemoteAdapter(R.id.widget_listview, fillData);
+            views.setEmptyView(R.id.widget_listview, R.id.widget_empty);
+
             // Click on header to launch main activity
             Intent intent = new Intent(context, MyStocksActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
             views.setOnClickPendingIntent(R.id.widget_header, pendingIntent);
 
-            // Set up the collection data (ListView)
-            views.setRemoteAdapter(R.id.widget_listview, new Intent(context, StocksWidgetService.class));
-            views.setEmptyView(R.id.widget_listview, R.id.widget_empty);
-            appWidgetManager.updateAppWidget(wid, views);
+            // Set the RemoteView for the Widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+
+        // Invalidate its old Collection data, so it can be repopulated
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_listview);
     }
 
     @Override
@@ -41,6 +57,5 @@ public class StocksWidgetProvider extends AppWidgetProvider {
             int[] wids = manager.getAppWidgetIds(new ComponentName(context, getClass()));
             manager.notifyAppWidgetViewDataChanged(wids, R.id.widget_listview);
         }
-
     }
 }
--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/StocksWidgetService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StocksWidgetService.java
@@ -1,8 +1,15 @@
 package com.sam_chordas.android.stockhawk.widget;
 
+import android.annotation.TargetApi;
+import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Binder;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
@@ -33,7 +40,7 @@ public class StocksWidgetService extends RemoteViewsService {
     static final int INDEX_QUOTES_ISUP = 5;
 
     @Override
-    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+    public RemoteViewsFactory onGetViewFactory(final Intent intent) {
         return new RemoteViewsFactory() {
             private Cursor data = null;
 
@@ -75,16 +82,27 @@ public class StocksWidgetService extends RemoteViewsService {
             public RemoteViews getViewAt(int position) {
                 if (position == AdapterView.INVALID_POSITION || data == null || !data.moveToPosition(position))
                     return null;
+
                 // Set data into each views
                 RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget_stocks_listitem);
                 views.setTextViewText(R.id.stock_symbol, data.getString(INDEX_QUOTES_SYMBOL));
                 views.setTextViewText(R.id.bid_price, data.getString(INDEX_QUOTES_BIDPRICE));
                 views.setTextViewText(R.id.change, data.getString(INDEX_QUOTES_PERCENT_CHANGE));
+
                 // Set Color for Pill background
                 if (data.getInt(INDEX_QUOTES_ISUP) == 1)
                     views.setInt(R.id.change, "setBackgroundResource", R.drawable.percent_change_pill_green);
                 else
                     views.setInt(R.id.change, "setBackgroundResource", R.drawable.percent_change_pill_red);
+
+                // Show or Hide the Bid-price on Long/Short Widget Width
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getApplicationContext());
+                int widgetWidth = getWidgetWidth(appWidgetManager, intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, 0));
+                int defaultWidth = getResources().getDimensionPixelSize(R.dimen.widget_stocks_default_width);
+                if (widgetWidth < defaultWidth)
+                    views.setViewVisibility(R.id.bid_price, View.GONE);
+                else
+                    views.setViewVisibility(R.id.bid_price, View.VISIBLE);
 
                 return views;
             }
@@ -110,6 +128,32 @@ public class StocksWidgetService extends RemoteViewsService {
             public boolean hasStableIds() {
                 return true;
             }
+
+            // Code in 2 methods below are from https://github.com/udacity/Advanced_Android_Development
+            private int getWidgetWidth(AppWidgetManager appWidgetManager, int appWidgetId) {
+                // Prior to Jelly Bean, widgets were always their default size
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    return getResources().getDimensionPixelSize(R.dimen.widget_stocks_default_width);
+                }
+                // For Jelly Bean and higher devices, widgets can be resized - the current size can be
+                // retrieved from the newly added App Widget Options
+                return getWidgetWidthFromOptions(appWidgetManager, appWidgetId);
+            }
+
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            private int getWidgetWidthFromOptions(AppWidgetManager appWidgetManager, int appWidgetId) {
+                Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+                if (options.containsKey(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)) {
+                    int minWidthDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+                    // The width returned is in dp, but we'll convert it to pixels to match the other widths
+                    DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+                    return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, minWidthDp,
+                            displayMetrics);
+                }
+                return  getResources().getDimensionPixelSize(R.dimen.widget_stocks_default_width);
+            }
+
         };
+
     }
 }
